bb5c2d1ca12a1dc22d59242ddf14349caeaf4043
Kevin Chevalier <kchevalier@protonmail.com>
2021-01-22 18:56:21 -0800

    add test to Format.number()

--- application/test/unit/lib/Format.test.js
+++ application/test/unit/lib/Format.test.js
@@ -7,7 +7,12 @@ import Format from '../../../src/lib/Format';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Format", () => {
+
   it("should format a number with a thousands separator", () => {
     expect(Format.number(8192)).to.equal("8,192");
-  })
+  });
+
+  it("should format a number with a thousands separator and three fractional digits", () => {
+    expect(Format.number(52197.925853, 3)).to.equal("52,197.926");
+  });
 });
